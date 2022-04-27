3fa94e27093282b9a1dd7313fb25d2a78c84d626
mesutBE <gelukkig.zijn.be@gmail.com>
2022-04-27 13:58:46 +0200

    Added button animation and refactor DOM to jquery

--- js/script.js
+++ js/script.js
@@ -1,15 +1,12 @@
-const nav = document.getElementById("myNav");
-const openBtn = document.getElementById("open-btn");
-const closeBtn = document.getElementById("close-btn");
+$('.navTrigger').click(function(){
+  $(this).toggleClass('active');
+  
+  $('.myNav').css("display") == "block" 
+  ? $('.myNav').css("display", "none") 
+  : $('.myNav').css("display", "block");
+});
 
-function openNav() {
-  nav.style.opacity = "1";
-  openBtn.style.display = "none";
-  closeBtn.style.display = "block";
-}
-
-function closeNav() {
-  nav.style.opacity = "0";
-  openBtn.style.display = "block";
-  closeBtn.style.display = "none";
-}
+$('.overlay-content').click(function(){
+  $('.myNav').css("display", "none");
+  $('.navTrigger').toggleClass('active');
+});
